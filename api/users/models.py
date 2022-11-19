b8c7e077dcd682234714a4c1592a35c551263e24
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-11-19 10:33:13 -0600

    feat(users): 'models.py' module was modified
    'User' model was added

---
+++ api/users/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from peewee import(
+    Model,
+    CharField,
+    DateTimeField,
+    BooleanField
+)
+
+
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
+class User(Model):
+    
+    username = CharField(max_length=20)
+    name = CharField(max_length=20)
+    password = CharField(max_length=70)
+    email = CharField(max_length=20)
+    creation_date = DateTimeField(default=datetime.now)
+    is_active = BooleanField(default=True)
