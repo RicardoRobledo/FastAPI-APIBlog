2c589e6c8ace1921fbbd6e06bccf9dc9a4f8360d
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-11-19 11:19:51 -0600

    feat(users): 'models.py' module was modified
    Fields in 'User' model were edited to have more length

--- api/users/models.py
+++ api/users/models.py
@@ -21,10 +21,10 @@ __version__ = '0.1'
 
 class User(Model):
     
-    username = CharField(max_length=20)
-    name = CharField(max_length=20)
+    username = CharField(max_length=70)
+    name = CharField(max_length=70)
     password = CharField(max_length=70)
-    email = CharField(max_length=20)
+    email = CharField(max_length=70)
     creation_date = DateTimeField(default=datetime.now)
     is_active = BooleanField(default=True)
     
